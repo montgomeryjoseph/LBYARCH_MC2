4c1b51b7a948df1c9ed4cdb1777081f0ab414ecf
ReythemGespenst <roncajumban@gmail.com>
2024-12-02 23:18:40 +0800

    compile then build the c file pls

--- CController.c
+++ CController.c
@@ -25,7 +25,7 @@ void calculate_distance(int n, double *x1, double *x2, double *y1, double *y2, d
 extern void calculateDist(int n, double *x1, double *x2, double *y1, double *y2, double *z);
 
 int main(){
-	int n = 30;
+	int n = 268435456;
     double *x1 = (double*)malloc(n*sizeof(double));
     double *x2 = (double*)malloc(n*sizeof(double));
     double *y1 = (double*)malloc(n*sizeof(double));
@@ -40,27 +40,34 @@ int main(){
     srand((unsigned int)time(NULL));
 
     for(int i = 0; i < n; i++){
-        x1[i] = (rand() / (double)RAND_MAX) * (max - min) + min;
-        x2[i] = (rand() / (double)RAND_MAX) * (max - min) + min;
-        y1[i] = (rand() / (double)RAND_MAX) * (max - min) + min;
-        y2[i] = (rand() / (double)RAND_MAX) * (max - min) + min;
+        x1[i] = (rand() / (double)RAND_MAX) * (50 - (-50)) -50;
+        x2[i] = (rand() / (double)RAND_MAX) * (50 - (-50)) -50;
+        y1[i] = (rand() / (double)RAND_MAX) * (50 - (-50)) -50;
+        y2[i] = (rand() / (double)RAND_MAX) * (50 - (-50)) -50;
         
     }
 
-    //calculate_distance(n, x1,x2,y1,y2,z);
 
-    //int i;
-    //for(i = 0; i < n; i++){
-    //	printf("%f\n", z[i]);
-    //}
+    //C kernel
+    clock_t start = clock();
+    calculate_distance(n, x1,x2,y1,y2,z);
+    clock_t end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time ran: %f\n", time_taken);
 
-    //free(z);
+    int i;
+    for(i = 0; i < 10; i++){
+    	printf("Point A: (%.8f, %.8f) Point B : (%.8f, %.8f) Z[%d] = %.8f\n", x1[i], y1[i], x2[i], y2[i], i, z[i]);
+    }
 
+    //ASM kernel
+    start = clock();
     calculateDist(n,x1,x2,y1,y2,z);
+    end = clock();
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time ran: %f\n", time_taken);
 
-    //printf("\n");
-
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < 10; i++){
     	printf("Point A: (%.8f, %.8f) Point B : (%.8f, %.8f) Z[%d] = %.8f\n", x1[i], y1[i], x2[i], y2[i], i, z[i]);
     }
 
